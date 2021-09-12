663d8a5b6a0740aee0c56299fe4f2d10cca7cab9
Mohammed Gamal Al-homaidi <90135545+MhdGmal1998@users.noreply.github.com>
2021-09-12 15:59:28 +0300

    the api class library for project

---
+++ AddEmpolyeeAPI/IEmpolyee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddEmpolyeeAPI
+{
+    public interface IEmpolyee
+    {
+        int Id { get; set; }
+        string Name { get; set; }
+        decimal Salary { get; set; }
+    }
+}
