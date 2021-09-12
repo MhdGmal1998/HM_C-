0076ae9d5ac1ee5d14ec392d03394306bf4165a0
Mohammed Gamal Al-homaidi <90135545+MhdGmal1998@users.noreply.github.com>
2021-09-12 16:01:29 +0300

    this is the executable projects for the main project

---
+++ AddEmpolyeeForm/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿using System.Windows.Forms;
+using System.Drawing;
+namespace AddEmpolyeeForm
+{
+    partial class Form1:Form
+    {
+        // The required Buttons for desinger 
+        private Button BtnAddEmpolyee;
+        private Button BtnClear;
+
+        // The required TextBox 
+        private TextBox TxtEmpolyeeId;
+        private TextBox TxtEmpolyeeName;
+        private TextBox TxtEmpolyeeSalary;
+
+        // The required ComboBox
+        private ComboBox CmbEmpolyeeType;
+
+        // The required Labels
+        private Label lblHeader;
+        private Label lblEmpolyeeCount;
+        private Label lblEmpolyeeSalary;
+        private Label lblEmpolyeeId;
+        private Label lblEpolyeeName;
+        private Label lblEpolyeeType;
+        private Label lblEmpolyeeSalariesSum;
+        private Label lblEmpolyeeCountHint;
+        private Label lblEmpolyeeSalariesSumnHint;
+        // ----------------------------------------//
+        private System.ComponentModel.IContainer components = null;
+    
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+       
+        private void InitializeComponent()
+        {
+            
+            SuspendLayout();
+            /*The Required Button For Desinger */
+            // 
+            // BtnAddEmpolyee
+            // 
+            BtnAddEmpolyee = new Button();
+            BtnAddEmpolyee.Location = new Point(405, 275);
+            BtnAddEmpolyee.Size = new Size(100, 50);
+            BtnAddEmpolyee.Text = "Add";
+            BtnAddEmpolyee.Click += AddEmpolyeeClick;
+            Controls.Add(BtnAddEmpolyee);
+            // 
+            // BtnClear
+            // 
+            BtnClear = new Button();
+            BtnClear.Location = new Point(500, 275);
+            BtnClear.Size = new Size(100, 50);
+            BtnClear.Text = "Clear";
+            Controls.Add(BtnClear);
+
+            /*The Required TextBox For Designer*/
+            // 
+            // TxtEmpolyeeId
+            // 
+
+            TxtEmpolyeeId = new TextBox();
+            TxtEmpolyeeId.Location = new Point(200, 80);
+            TxtEmpolyeeId.Size = new Size(200, 37);
+            Controls.Add(TxtEmpolyeeId);
+
+            // 
+            // TxtEmpolyeeName
+            // 
+            TxtEmpolyeeName = new TextBox();
+            TxtEmpolyeeName.Location = new Point(200, 130);
+            TxtEmpolyeeName.Size = new Size(200, 37);
+            Controls.Add(TxtEmpolyeeName);
+
+            /*The Required Combox For Label*/
+            // 
+            // CmbEmpolyeeType
+            // 
+            CmbEmpolyeeType = new ComboBox();
+            CmbEmpolyeeType.Location = new Point(200, 180);
+            CmbEmpolyeeType.Size = new Size(200, 37);
+            Controls.Add(CmbEmpolyeeType);
+
+            // 
+            // TxtEmpolyeeSalary
+            // 
+            TxtEmpolyeeSalary = new TextBox();
+            TxtEmpolyeeSalary.Location = new Point(200, 230);
+            TxtEmpolyeeSalary.Size = new Size(200, 37);
+            Controls.Add(TxtEmpolyeeSalary);
+
+
+
+
+
+            /*The Required Label */
+            //
+            // lblHeader
+            //
+            lblHeader = new Label();
+            lblHeader.Text = "Empolyee Application ";
+            lblHeader.AutoSize = true;
+            lblHeader.Location = new Point(Width/2,10);
+            Controls.Add(lblHeader);
+
+            // 
+            // lblEmpolyeeId
+            // 
+            lblEmpolyeeId = new Label();
+            lblEmpolyeeId.AutoSize = true;
+            lblEmpolyeeId.Location = new System.Drawing.Point(10, 80);
+            lblEmpolyeeId.Size = new System.Drawing.Size(118, 29);
+            lblEmpolyeeId.Text = "Empolyee Id";
+            Controls.Add(lblEmpolyeeId);
+
+            // 
+            // lblEpolyeeName
+            // 
+            lblEpolyeeName = new Label();
+            lblEpolyeeName.Location = new Point(10, 130);
+            lblEpolyeeName.Size = new Size(168, 23);
+            lblEpolyeeName.Text = "Empolyee Name";
+            Controls.Add(lblEpolyeeName);
+
+            // 
+            // lblEpolyeeType
+            // 
+            lblEpolyeeType = new Label();
+            lblEpolyeeType.Location = new System.Drawing.Point(10, 180);
+            lblEpolyeeType.Size = new System.Drawing.Size(100, 23);
+            lblEpolyeeType.Text = "Empolyee Type";
+            lblEpolyeeType.AutoSize = true;
+            Controls.Add(lblEpolyeeType);
+
+            // lblEmpolyeeSalary
+            lblEmpolyeeSalary = new Label();
+            lblEmpolyeeSalary.AutoSize = true;
+            lblEmpolyeeSalary.Text = "Empolyee Salary";
+            lblEmpolyeeSalary.Location = new Point(10, 230);
+            Controls.Add(lblEmpolyeeSalary);
+
+            // 
+            // lblEmpolyeeCount
+            // 
+            lblEmpolyeeCount = new Label();
+            lblEmpolyeeCount.Location = new Point(200, 300);
+            lblEmpolyeeCount.Size = new Size(100, 23);
+            lblEmpolyeeCount.Text = "0";
+            Controls.Add(lblEmpolyeeCount);
+
+            // 
+            // lblEmpolyeeSalariesSum
+            // 
+            lblEmpolyeeSalariesSum = new Label();
+            lblEmpolyeeSalariesSum.Location = new Point(200, 350);
+            lblEmpolyeeSalariesSum.AutoSize = true;
+            lblEmpolyeeSalariesSum.Text = "0.00";
+            Controls.Add(lblEmpolyeeSalariesSum);
+
+            // lblEmpolyeeCountHint
+            lblEmpolyeeCountHint = new Label();
+            lblEmpolyeeCountHint.Location = new Point(10,300);
+            lblEmpolyeeCountHint.Text = "Empolyee Count :";
+            lblEmpolyeeCountHint.AutoSize = true;
+            Controls.Add(lblEmpolyeeCountHint);
+
+            // lblEmpolyeeSalariesSumnHint
+            lblEmpolyeeSalariesSumnHint = new Label();
+            lblEmpolyeeSalariesSumnHint.Text = "Empolyee Salaries :";
+            lblEmpolyeeSalariesSumnHint.Location = new Point(10, 350);
+            lblEmpolyeeSalariesSumnHint.AutoSize = true;
+            Controls.Add(lblEmpolyeeSalariesSumnHint);
+            //
+            // Form1
+            // 
+          
+            Font = new Font("Adobe Arabic", 20F, FontStyle.Bold);
+            Text = "Empolyee Application";
+            Size = new Size(620,450);
+            ResumeLayout(false);
+            PerformLayout();
+            AutoSize = true;
+            
+        }
+
+        #endregion
+    }
+}
---
+++ AddEmpolyeeForm/Form1.cs
@@ -0,0 +1,152 @@
+﻿using AddEmpolyeeAPI;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AddEmpolyeeForm
+{
+    public enum EmpolyeeType
+    {
+        Teacher,
+        HeadMaster,
+        HeadOfDepartment
+    }
+    public partial class Form1 : Form
+    {
+        List<IEmpolyee> empolyee = new List<IEmpolyee>();
+        public Form1()
+        {
+            
+            InitializeComponent();
+            CmbEmpolyeeType.DataSource = Enum.GetValues(typeof(EmpolyeeType));
+
+        }
+
+        private void ClearEmpolyeeClick(Object sender,EventArgs arg)
+        {
+
+        }
+        private void AddEmpolyeeClick(Object sender,EventArgs arg)
+        {
+            //if(CmbEmpolyeeType.Text.ToString()=="Teacher")
+            //{
+            //    IEmpolyee teacher = new Teacher
+            //    {
+            //        Id = Convert.ToInt32(TxtEmpolyeeId.Text),
+            //        Name = TxtEmpolyeeName.Text,
+            //        Salary = Convert.ToDecimal(TxtEmpolyeeSalary.Text)+(Convert.ToDecimal(TxtEmpolyeeSalary.Text)*0.05m)
+            //    };
+            //    empolyee.Add(teacher);
+            //}
+            //else if(CmbEmpolyeeType.Text.ToString() == "HeadMaster")
+            //{
+            //    IEmpolyee headmaster = new HeadMaster
+            //    {
+            //        Id = Convert.ToInt32(TxtEmpolyeeId.Text),
+            //        Name = TxtEmpolyeeName.Text,
+            //        Salary = Convert.ToDecimal(TxtEmpolyeeSalary.Text) + (Convert.ToDecimal(TxtEmpolyeeSalary.Text) * 0.10m)
+            //    };
+            //    empolyee.Add(headmaster);
+            //}
+            //else
+            //{
+            //    IEmpolyee headofdepartment = new HeadOfDepartment
+            //    {
+            //        Id = Convert.ToInt32(TxtEmpolyeeId.Text),
+            //        Name = TxtEmpolyeeName.Text,
+            //        Salary = Convert.ToDecimal(TxtEmpolyeeSalary.Text) + (Convert.ToDecimal(TxtEmpolyeeSalary.Text) * 0.15m)
+            //    };
+            //    empolyee.Add(headofdepartment);
+            //}
+
+            //lblEmpolyeeCount.Text = empolyee.Count.ToString();
+            //decimal SumEmpolyeeSalaries = 0;
+            //foreach (IEmpolyee ei in empolyee)
+            //{
+            //    SumEmpolyeeSalaries += ei.Salary;
+            //}
+            //lblEmpolyeeSalariesSum.Text = SumEmpolyeeSalaries.ToString();
+
+            if(CmbEmpolyeeType.Text.Equals(EmpolyeeType.Teacher.ToString()))
+            {
+                IEmpolyee teacher = EmployeeFactory.GetEmpolyeeInstance(EmpolyeeType.Teacher, Convert.ToInt32(TxtEmpolyeeId.Text), TxtEmpolyeeName.Text, Convert.ToDecimal(TxtEmpolyeeSalary.Text));
+                empolyee.Add(teacher);
+            }
+            else if(CmbEmpolyeeType.Text.Equals(EmpolyeeType.HeadOfDepartment.ToString()))
+            {
+                IEmpolyee headofdepartment = EmployeeFactory.GetEmpolyeeInstance(EmpolyeeType.HeadOfDepartment, Convert.ToInt32(TxtEmpolyeeId.Text), TxtEmpolyeeName.Text, Convert.ToDecimal(TxtEmpolyeeSalary.Text));
+                empolyee.Add(headofdepartment);
+            }
+            else
+            {
+                IEmpolyee headmaster = EmployeeFactory.GetEmpolyeeInstance(EmpolyeeType.HeadMaster, Convert.ToInt32(TxtEmpolyeeId.Text), TxtEmpolyeeName.Text, Convert.ToDecimal(TxtEmpolyeeSalary.Text));
+                empolyee.Add(headmaster);
+            }
+           
+            lblEmpolyeeCount.Text = empolyee.Count.ToString();
+            lblEmpolyeeSalariesSum.Text = empolyee.Sum(e => e.Salary).ToString();
+        //    MessageBox.Show(CmbEmpolyeeType.Text);
+        }
+    }
+
+    public class Teacher : EmpolyeeBase
+    {
+         public override decimal Salary
+        {
+            get
+            {
+                return (base.Salary + (base.Salary*0.03m));
+            }
+        }
+    }
+
+    public class HeadMaster : EmpolyeeBase
+    {
+        public override decimal Salary
+        {
+            get
+            {
+                return (base.Salary + (base.Salary * 0.10m));
+            }
+        }
+    }
+    public class HeadOfDepartment : EmpolyeeBase
+    {
+        public override decimal Salary
+        {
+            get
+            {
+                return (base.Salary + (base.Salary * 0.15m));
+            }
+        }
+    }
+
+    public static class EmployeeFactory
+    {
+        public static IEmpolyee GetEmpolyeeInstance(EmpolyeeType empolyeetype, int id, string name,decimal salary)
+        {
+            IEmpolyee empolyee = null;
+            switch(empolyeetype)
+            {
+                case EmpolyeeType.Teacher:
+                    empolyee = new Teacher { Id = id, Name = name, Salary = salary };
+                    break;
+                case EmpolyeeType.HeadOfDepartment:
+                    empolyee = new HeadOfDepartment { Id = id, Name = name, Salary = salary };
+                    break;
+                case EmpolyeeType.HeadMaster:
+                    empolyee = new HeadMaster { Id = id, Name = name, Salary = salary };
+                    break;
+                default:
+                    break;
+            }
+            return empolyee;
+        }
+    }
+}
